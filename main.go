eaa4bd40a22b6089e19b0ba76bcf28948e105110
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-03-22 16:19:23 +0300

    get need data

--- main.go
+++ main.go
@@ -4,19 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type Cryptorank struct {
+	Name      string
+	Teg       string
+	Timestrap time.Time
+}
+
 func main() {
-
 	webPage := "https://cryptorank.io/"
-	resp, err := http.Get(webPage)
 
+	resp, err := http.Get(webPage)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
@@ -24,11 +29,25 @@ func main() {
 	}
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	title := doc.Find("title").Text()
-	fmt.Println(title)
+	rows := make([]Cryptorank, 0)
+	data := doc.Find(".data-table__table-content")
+	data.First().Find("tbody").Find("tr").Each(func(iх int, sel *goquery.Selection) {
+		row := new(Cryptorank)
+		sel.Find("span").Each(func(i int, selection *goquery.Selection) {
+			switch i {
+			case 0:
+				row.Name = selection.Text()
+			case 1:
+				row.Teg = selection.Text()
+			case 3:
+				row.Timestrap = time.Now()
+			}
+		})
+		rows = append(rows, *row)
+	})
+	fmt.Println(rows)
 }
